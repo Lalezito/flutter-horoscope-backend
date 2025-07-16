bbfe1837bdfe240217ea96a5bce8ec8ab082d3a5
Alejandro Caceres <laleshootingart@gmail.com>
2025-07-16 18:12:12 +1200

    Update app.js: use cors from npm and dotenv

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const cors = require("./middleware/cors");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const coachingRoutes = require("./routes/coaching");
 
+dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(cors);
 
 app.use("/api/coaching", coachingRoutes);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("🚀 Servidor corriendo en Railway");
 });
